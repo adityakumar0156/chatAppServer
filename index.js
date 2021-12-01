5966b86d06832065e2a96b321efe4e32c1975482
Aditya Kumar <73734281+adityakumar0156@users.noreply.github.com>
2021-12-01 22:22:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//node server which will handle socket io connections
+const io = require('socket.io')(8000);
+const users = {};
+io.on('connection', socket => {
+    socket.on("new-user-joined", name => {
+        console.log("new-user-joined", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', (message) => {
+        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', (message) => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    })
+    socket.on('type', (name) => {
+        console.log(name, ' is typing');
+        socket.broadcast.emit('typing', users[socket.id]);
+    })
+
+})
